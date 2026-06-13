f22d7430e3a775491b100df6adda13b65c450e3b
Matej <gabrhelmatej@gmail.com>
2026-06-13 13:23:45 +0200

    Black jack a hit asi dokončen

--- black-jack/Main.cs
+++ black-jack/Main.cs
@@ -1,10 +1,19 @@
 
-Player hrac = new Player(0);
+Player hrac = new Player(0 + Random.Shared.Next(1, 12)+ Random.Shared.Next(1, 12));
 Rozdavac rozdavac = new Rozdavac(0 + Random.Shared.Next(1, 12));
 Karty karty = new Karty(0);
 
+
+
 while (true)
 {
+    if (hrac.Skore == 21)
+    {
+        Console.WriteLine("Dostal jste: " +  hrac.Skore);
+        Console.WriteLine("!!BLACK JACK!!");
+        break;
+    }
+    
     Console.WriteLine("Rozdavac::  " + rozdavac.Skore);
     Console.WriteLine("Hrac::  " + hrac.Skore);
     Console.WriteLine("");
@@ -15,5 +24,19 @@ while (true)
     if (input == "hit" || input == "h")
     {
         hrac.Skore = hrac.Skore + karty.Hit();
+
+        if (hrac.Skore == 21)
+        {
+            Console.WriteLine("Dostal jste: " +  hrac.Skore);
+            Console.WriteLine("Winner winner chicken dinner!!");
+            break;
+        }
+
+        if (hrac.Skore > 21)
+        {
+            Console.WriteLine("Dostal jste: " +  hrac.Skore);
+            Console.WriteLine("Game Over");
+            break;
+        }
     }
 }
